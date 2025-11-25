ea76b85affd85da41fdcea356b0adfd3236837da
Inadomi Takayuki <128318073+e224204it@users.noreply.github.com>
2025-11-25 16:25:42 +0900

    Refactor data loading to use Promise.all
    Refactor loadJsonData function to loadFoods using Promise.all for concurrent fetching.

--- js/loadData.js
+++ js/loadData.js
@@ -1,15 +1,13 @@
-async function loadJsonData(path) {
-    const response = await fetch(path);
-    return await response.json();
+// Promiseを返す関数として定義
+export async function loadFoods() {
+    const [foodsOne, foodsTwo, foodsThree, foodsFour, foodsFive, rice, diiParam] = await Promise.all([
+        fetch("../data/foods_data1.json").then(res => res.json()),
+        fetch("../data/foods_data2.json").then(res => res.json()),
+        fetch("../data/foods_data3.json").then(res => res.json()),
+        fetch("../data/foods_data4.json").then(res => res.json()),
+        fetch("../data/foods_data5.json").then(res => res.json()),
+        fetch("../data/fortifiedRice.json").then(res => res.json()),
+        fetch("../data/dii_param.json").then(res => res.json())
+    ]);
+    return { foodsOne, foodsTwo, foodsThree, foodsFour, foodsFive, rice, diiParam };
 }
-
-const foodsOne = await loadJsonData("./data/foods_data1.json");
-const foodsTwo = await loadJsonData("./data/foods_data2.json");
-const foodsThree = await loadJsonData("./data/foods_data3.json");
-const foodsFour = await loadJsonData("./data/foods_data4.json");
-const foodsFive = await loadJsonData("./data/foods_data5.json");
-const rice = await loadJsonData("./data/fortifiedRice.json");
-
-console.log(foodsOne)
-
-export { foodsOne, foodsTwo, foodsThree, foodsFour, foodsFive, rice }
